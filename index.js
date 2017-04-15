27f7d8bb8fad54ed7ad19816617042aefdaf62e9
lcorr8 <l.corr.v@gmail.com>
2017-04-15 19:36:44 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+import {message} from './components/foo'
+
+
+// In a file in the same directory as tweet.js
+import Tweet from './Tweet'
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('main')
+);
